2943b753624b5affb080b4aeec565da7a76ef927
fng <daniel.wydler@gmail.com>
2021-12-06 06:11:38 +0100

    Day 05 - Part 2

--- src/main/scala/com/github/fng/adventofcode2021/day05/Day05.scala
+++ src/main/scala/com/github/fng/adventofcode2021/day05/Day05.scala
@@ -9,12 +9,10 @@ object Day05 {
 
       val maxX = map.keys.maxBy(_.x)
       val maxY = map.keys.maxBy(_.y)
-      val matrix = 0.to(maxY.y).toList.map{
-        y =>
-          0.to(maxX.x).toList.map{
-            x =>
-              map.get(Point(x, y)).map(_.toString).getOrElse(".")
-          }
+      val matrix = 0.to(maxY.y).toList.map { y =>
+        0.to(maxX.x).toList.map { x =>
+          map.get(Point(x, y)).map(_.toString).getOrElse(".")
+        }
       }
       matrix.map(_.mkString).mkString("\n")
     }
@@ -25,42 +23,85 @@ object Day05 {
 
   }
 
-  def parseInput(input: List[String]) : List[((Point, Point))] = {
+  def parseInput(input: List[String]): List[((Point, Point))] = {
     input.map { line =>
       line.split(" -> ").toList match {
         case List(a, b) => (parsePoint(a), parsePoint(b))
-        case other => sys.error(s"can't parse line: $line")
+        case other      => sys.error(s"can't parse line: $line")
       }
     }
   }
 
   private def parsePoint(point: String): Point = point.split(",").toList match {
     case List(x, y) => Point(x.toInt, y.toInt)
-    case other => sys.error(s"Can't parse point: $point")
+    case other      => sys.error(s"Can't parse point: $point")
   }
 
-
-  def generateLinePoints(from: Point, to: Point): List[Point] = {
-    if(from.x == to.x){
-      //vertical line
-      if(from.y < to.y) from.y.to(to.y).map(y => from.copy(y = y)).toList
-      else to.y.to(from.y).map(y => from.copy(y = y)).toList
-    } else if (from.y == to.y) {
-     //horizontal line
-      if(from.x < to.x) from.x.to(to.x).map(x => from.copy(x = x)).toList
-      else to.x.to(from.x).map(x => from.copy(x = x)).toList
-    } else Nil
-  }
-
-  def generateDiagram(fromToPoints: List[(Point, Point)]): Diagram = {
-
-    val map = fromToPoints.flatMap(fromTo => generateLinePoints(fromTo._1, fromTo._2)).foldLeft(Map[Point, Int]()){
-      case (map, point) =>
-        map.updatedWith(point){old => old.map(_ + 1).orElse(Some(1))}
+  object Part1 {
+    def generateLinePoints(from: Point, to: Point): List[Point] = {
+      if (from.x == to.x) {
+        //vertical line
+        if (from.y < to.y) from.y.to(to.y).map(y => from.copy(y = y)).toList
+        else to.y.to(from.y).map(y => from.copy(y = y)).toList
+      } else if (from.y == to.y) {
+        //horizontal line
+        if (from.x < to.x) from.x.to(to.x).map(x => from.copy(x = x)).toList
+        else to.x.to(from.x).map(x => from.copy(x = x)).toList
+      } else Nil
     }
-    Diagram(map)
 
+    def generateDiagram(fromToPoints: List[(Point, Point)]): Diagram = {
+      val map = fromToPoints
+        .flatMap(fromTo => generateLinePoints(fromTo._1, fromTo._2))
+        .foldLeft(Map[Point, Int]()) { case (map, point) =>
+          map.updatedWith(point) { old => old.map(_ + 1).orElse(Some(1)) }
+        }
+      Diagram(map)
+    }
+  }
 
+  object Part2 {
+    def generateLinePoints(from: Point, to: Point): List[Point] = {
+      if (from.x == to.x) {
+        //vertical line
+        if (from.y < to.y) from.y.to(to.y).map(y => from.copy(y = y)).toList
+        else to.y.to(from.y).map(y => from.copy(y = y)).toList
+      } else if (from.y == to.y) {
+        //horizontal line
+        if (from.x < to.x) from.x.to(to.x).map(x => from.copy(x = x)).toList
+        else to.x.to(from.x).map(x => from.copy(x = x)).toList
+      } else {
+        //diagonal
+
+        val xSteps = to.x - from.x
+        val ySteps = to.y - from.y
+
+        if (xSteps.abs != ySteps.abs)
+          sys.error(
+            s"number of steps on x and y axis need to be the same for diagonal movement: " +
+              s"xSteps: $xSteps, ySteps: $ySteps"
+          )
+
+        val xStepMultiplier = if (xSteps > 0) 1 else -1
+        val yStepMultiplier = if (ySteps > 0) 1 else -1
+
+        0.to(xSteps.abs).toList.map { step =>
+          Point(
+            from.x + step * xStepMultiplier,
+            from.y + step * yStepMultiplier
+          )
+        }
+      }
+    }
+
+    def generateDiagram(fromToPoints: List[(Point, Point)]): Diagram = {
+      val map = fromToPoints
+        .flatMap(fromTo => generateLinePoints(fromTo._1, fromTo._2))
+        .foldLeft(Map[Point, Int]()) { case (map, point) =>
+          map.updatedWith(point) { old => old.map(_ + 1).orElse(Some(1)) }
+        }
+      Diagram(map)
+    }
   }
 
 }
--- src/test/scala/com/github/fng/adventofcode2021/day05/Day05Test.scala
+++ src/test/scala/com/github/fng/adventofcode2021/day05/Day05Test.scala
@@ -13,20 +13,22 @@ class Day05Test extends AnyFunSuite {
 
     val fromToPoints = Day05.parseInput(input)
 
-    assert(fromToPoints === List(
-      (Point(0,9),Point(5,9)),
-        (Point(8,0),Point(0,8)),
-        (Point(9,4),Point(3,4)),
-        (Point(2,2),Point(2,1)),
-        (Point(7,0),Point(7,4)),
-        (Point(6,4),Point(2,0)),
-        (Point(0,9),Point(2,9)),
-        (Point(3,4),Point(1,4)),
-        (Point(0,0),Point(8,8)),
-        (Point(5,5),Point(8,2))
-    ))
+    assert(
+      fromToPoints === List(
+        (Point(0, 9), Point(5, 9)),
+        (Point(8, 0), Point(0, 8)),
+        (Point(9, 4), Point(3, 4)),
+        (Point(2, 2), Point(2, 1)),
+        (Point(7, 0), Point(7, 4)),
+        (Point(6, 4), Point(2, 0)),
+        (Point(0, 9), Point(2, 9)),
+        (Point(3, 4), Point(1, 4)),
+        (Point(0, 0), Point(8, 8)),
+        (Point(5, 5), Point(8, 2))
+      )
+    )
 
-    val diagram = Day05.generateDiagram(fromToPoints)
+    val diagram = Day05.Part1.generateDiagram(fromToPoints)
     println(diagram)
 
     assert(diagram.score === 5)
@@ -38,12 +40,34 @@ class Day05Test extends AnyFunSuite {
     val input = ResourceUtils.getLinesFromResource("day05/input.txt")
 
     val fromToPoints = Day05.parseInput(input)
-    val diagram = Day05.generateDiagram(fromToPoints)
+    val diagram = Day05.Part1.generateDiagram(fromToPoints)
 
     assert(diagram.score === 5835)
 
   }
 
+  test("Day05 - Part2 - reference") {
 
+    val input = ResourceUtils.getLinesFromResource("day05/reference-input.txt")
+
+    val fromToPoints = Day05.parseInput(input)
+
+    val diagram = Day05.Part2.generateDiagram(fromToPoints)
+    println(diagram)
+
+    assert(diagram.score === 12)
+
+  }
+
+  test("Day05 - Part2 - exercise") {
+
+    val input = ResourceUtils.getLinesFromResource("day05/input.txt")
+
+    val fromToPoints = Day05.parseInput(input)
+
+    val diagram = Day05.Part2.generateDiagram(fromToPoints)
+
+    assert(diagram.score === 17013)
+  }
 
 }
